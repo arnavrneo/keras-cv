d634a26fa4160432acab8e18a61e850fc0b3218d
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-01-29 17:06:37 +0000

    Backbone prep

--- keras_cv/models/object_detection/yolov3/__internal__/layers/yolo_backbone.py
+++ keras_cv/models/object_detection/yolov3/__internal__/layers/yolo_backbone.py
@@ -5,21 +5,48 @@ from keras_cv.models.__internal__.darknet_utils import (
     ResidualBlocks,
 )
 
-class darknet53(keras.layers.Layer):
+# darknet53 body
 
+
+class darknet53_body(keras.layers.Layer):
+    ''' 52 conv2d layers '''
     def __init__(self):
         super().__init__()
-        self.conv1 = DarknetConvBlock(filters=(3, 3), strides=1)
-        self.conv2 = DarknetConvBlock(filters=(3, 3), strides=1)
-        self.residual = ResidualBlocks(1)
+        self.conv0 = DarknetConvBlock(filters=32, kernel_size=(3, 3), strides=1)
+        self.res0 = ResidualBlocks(64, 1)
+        self.res1 = ResidualBlocks(128, 2)
+        self.res2 = ResidualBlocks(256, 8)
+        self.res3 = ResidualBlocks(512, 8)
+        self.res4 = ResidualBlocks(1024, 4)
+    
+    def call(self, inputs):
+        x = self.conv0(inputs)
+        x = self.res0(inputs)
+        x = self.res1(inputs)
+        x = self.res2(inputs)
+        x = self.res3(inputs)
+        x = self.res4(inputs)
+        return x
 
-class darknetblock(keras.layers.Layer):
+
+class Yolov3Model(keras.layers.Layer):
     def __init__(
-        self
+        self,
+        in_channels=[256, 512, 1024],
+        depth_multiplier=1.0,
+        width_multiplier=1.0,
+        activation="silu"
     ):
         super().__init__()
-        
+        self.in_channels = in_channels
         # layers instantialize
+
+        self.conv0 = DarknetConvBlock(
+            filters=int(in_channels[1] * width_multiplier),
+            kernel_size=1,
+            strides=1,
+            activation=activation
+        )
     
     def call(self, inputs, training=False):
 
