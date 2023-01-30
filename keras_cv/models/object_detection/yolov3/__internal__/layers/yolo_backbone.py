d36890efda3609246c1b69536687668abb246cf6
Arnav <48650781+arnavrneo@users.noreply.github.com>
2023-01-30 17:39:13 +0000

    Darknet53 and Yolov3 block buildup

--- keras_cv/models/object_detection/yolov3/__internal__/layers/yolo_backbone.py
+++ keras_cv/models/object_detection/yolov3/__internal__/layers/yolo_backbone.py
@@ -8,45 +8,84 @@ from keras_cv.models.__internal__.darknet_utils import (
 # darknet53 body
 
 
-class darknet53_body(keras.layers.Layer):
+class Darknet53_body(keras.layers.Layer):
     ''' 52 conv2d layers '''
     def __init__(self):
         super().__init__()
-        self.conv0 = DarknetConvBlock(filters=32, kernel_size=(3, 3), strides=1)
-        self.res0 = ResidualBlocks(64, 1)
-        self.res1 = ResidualBlocks(128, 2)
-        self.res2 = ResidualBlocks(256, 8)
-        self.res3 = ResidualBlocks(512, 8)
-        self.res4 = ResidualBlocks(1024, 4)
+        self.conv0 = DarknetConvBlock(filters=32, kernel_size=(3, 3), strides=2)
+        self.conv1 = DarknetConvBlock(filters=64, kernel_size=(3, 3), strides=2)
+        self.res0 = ResidualBlocks(32, 1)
+        self.conv2 = DarknetConvBlock(filters=128, kernel_size=(3, 3), strides=2)
+        self.res1 = ResidualBlocks(64, 2)
+        self.conv3 = DarknetConvBlock(filters=256, kernel_size=(3, 3), strides=1)
+        self.res2 = ResidualBlocks(128, 8)
+        self.conv4 = DarknetConvBlock(filters=512, kernel_size=(3, 3), strides=1)
+        self.res3 = ResidualBlocks(256, 8)
+        self.conv5 = DarknetConvBlock(filters=1024, kernel_size=(3, 3), strides=1)
+        self.res4 = ResidualBlocks(512, 4)
     
     def call(self, inputs):
         x = self.conv0(inputs)
-        x = self.res0(inputs)
-        x = self.res1(inputs)
-        x = self.res2(inputs)
-        x = self.res3(inputs)
-        x = self.res4(inputs)
-        return x
+        x = self.conv1(x)
+        x = self.res0(x)
+        x = self.conv2(x)
+        x = self.res1(x)
+        x = self.conv3(x)
+        x = self.res2(x)
+
+        route1 = x   # 128
+
+        x = self.conv4(x)
+        x = self.res3(x)
+
+        route2 = x   # 256
+
+        x = self.conv5(x)
+        x = self.res4(x)
+
+        route3 = x   # 512
+        return route1, route2, route3
+
+
+class Yolov3_block(keras.layers.Layer):
+    def __init__(
+        self,
+        filters
+    ):
+        self.filters = filters
+        super().__init__()
+        self.conv0 = DarknetConvBlock(filters=filters, kernel_size=1, strides=1)
+        self.conv1 = DarknetConvBlock(filters=2*filters, kernel_size=3, strides=1)
+        self.conv2 = DarknetConvBlock(filters=filters, kernel_size=1, strides=1)
+        self.conv3 = DarknetConvBlock(filters=2*filters, kernel_size=3, strides=1)
+        self.conv4 = DarknetConvBlock(filters=filters, kernel_size=1, strides=1)
+        self.conv5 = DarknetConvBlock(filters=2*filters, kernel_size=3, strides=1)
+        
+    def call(self, inputs):
+        x = self.conv0(inputs)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        route = x
+
+        x = self.conv5(x)
+        return route, x
 
 
 class Yolov3Model(keras.layers.Layer):
     def __init__(
         self,
-        in_channels=[256, 512, 1024],
-        depth_multiplier=1.0,
-        width_multiplier=1.0,
-        activation="silu"
+        inputs,
+        num_anchors,
+        img_size
+
     ):
         super().__init__()
-        self.in_channels = in_channels
         # layers instantialize
-
-        self.conv0 = DarknetConvBlock(
-            filters=int(in_channels[1] * width_multiplier),
-            kernel_size=1,
-            strides=1,
-            activation=activation
-        )
+        self.res0 = ResidualBlocks(1024, 2)
+        
     
     def call(self, inputs, training=False):
 
